d7a28ac844c6c1315fdb69a35557ec6e892181e3
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-02-02 15:15:40 +0900

    Refactor analyzeFile command and update package.json; add Dispose interface

--- src/commands/analyzeFile.ts
+++ src/commands/analyzeFile.ts
@@ -1,8 +1,36 @@
 import * as vscode from 'vscode';
 
+class AnalyzeFile extends vscode.Disposable
+{
+    // add constructor
+    constructor(context: vscode.ExtensionContext) {
+        super(() => {
+            console.log('Dispose AnalyzeFile');
+        });
+
+        // Register the command
+        context.subscriptions.push(vscode.commands.registerCommand('php-do-not-overwrite-variable.analyzeActivatedFile', () => analyzeFile(context)));
+    }
+}
+
 const analyzeFile = async (context: vscode.ExtensionContext) => {
-    vscode.window.showInformationMessage('Hello VS Code');
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        const document = editor.document;
+        const text = document.getText();
+        
+        // Perform your analysis on the text
+        // For example, you can log the text to the console
+        console.log('Analyzing file:', document.fileName);
+        console.log('File content:', text);
+
+        // Display a message to the user
+        vscode.window.showInformationMessage(`Analyzed file: ${document.fileName}`);
+    } else {
+        vscode.window.showInformationMessage('No active editor found.');
+    }
 };
 
 
-export default analyzeFile;
+// export AnalyzeFile, analyzeFile both
+export { AnalyzeFile };
