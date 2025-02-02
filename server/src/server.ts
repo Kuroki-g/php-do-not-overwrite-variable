4f40efb9e53f4f292b8bd425e9979cec9b380e0b
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-02-02 23:49:28 +0900

    Comment out unused completion handlers and improve logging for file change events

--- server/src/server.ts
+++ server/src/server.ts
@@ -186,44 +186,44 @@ connection.languages.diagnostics.on(async (params) => {
 // 	return diagnostics;
 // }
 
+// Installs a handler for the DidChangeWatchedFiles notification.
 connection.onDidChangeWatchedFiles((_change) => {
-	// Monitored files have change in VSCode
-	connection.console.log("We received a file change event");
+	connection.console.log("onDidChangeWatchedFiles");
 });
 
 // This handler provides the initial list of the completion items.
-connection.onCompletion(
-	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-		// The pass parameter contains the position of the text document in
-		// which code complete got requested. For the example we ignore this
-		// info and always provide the same completion items.
-		return [
-			{
-				label: "TypeScript",
-				kind: CompletionItemKind.Text,
-				data: 1,
-			},
-			{
-				label: "JavaScript",
-				kind: CompletionItemKind.Text,
-				data: 2,
-			},
-		];
-	},
-);
+// connection.onCompletion(
+// 	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+// 		// The pass parameter contains the position of the text document in
+// 		// which code complete got requested. For the example we ignore this
+// 		// info and always provide the same completion items.
+// 		return [
+// 			{
+// 				label: "TypeScript",
+// 				kind: CompletionItemKind.Text,
+// 				data: 1,
+// 			},
+// 			{
+// 				label: "JavaScript",
+// 				kind: CompletionItemKind.Text,
+// 				data: 2,
+// 			},
+// 		];
+// 	},
+// );
 
 // This handler resolves additional information for the item selected in
 // the completion list.
-connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-	if (item.data === 1) {
-		item.detail = "TypeScript details";
-		item.documentation = "TypeScript documentation";
-	} else if (item.data === 2) {
-		item.detail = "JavaScript details";
-		item.documentation = "JavaScript documentation";
-	}
-	return item;
-});
+// connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+// 	if (item.data === 1) {
+// 		item.detail = "TypeScript details";
+// 		item.documentation = "TypeScript documentation";
+// 	} else if (item.data === 2) {
+// 		item.detail = "JavaScript details";
+// 		item.documentation = "JavaScript documentation";
+// 	}
+// 	return item;
+// });
 
 // Make the text document manager listen on the connection
 // for open, change and close text document events
